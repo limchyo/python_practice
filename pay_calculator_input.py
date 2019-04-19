25a77b8801961a561085f589c612c5119e174dd1
Chaeoh <limchyo>
2019-04-19 16:03:21 +0900

    pay_calculator_input.py upgrade

--- pay_calculator_input.py
+++ pay_calculator_input.py
@@ -9,16 +9,50 @@
 
 print("급여계산기 프로그램입니다!")
 
-pay = input("시급을 입력해주세요 : ")
-time_of_day = input("일일근무시간 : ")
-day_of_month = input("한달근무일수 : ")
+# pay = input("시급을 입력해주세요 : ")
+# time_of_day = input("일일근무시간 : ")
+# day_of_month = input("한달근무일수 : ")
 
-pay = int(pay)
-time_of_day = int(time_of_day)
-day_of_month = int(day_of_month)
+# pay = int(pay)
+# time_of_day = int(time_of_day)
+# day_of_month = int(day_of_month)
+
+pay = int(input("시급을 입력해주세요 : "))
+time_of_day = int(input("일일근무시간 : "))
+day_of_month = int(input("한달근무일수 : "))
+
+practice = int(input('''
+수습을 적용하나요?
+1. 적용
+2. 미적용
+'''))
+
+tax = int(input('''
+세금을 적용하나요?
+1. 미적용
+2. 4대 보혐료 공제
+3. 소득세 공제
+4. 모두 적용
+'''))
 
 # 월급
 profit = pay * time_of_day * day_of_month
+
+if practice == 1:
+    practice_price = profit // 10
+else:
+    practice_price = 0
+
+if tax == 2:
+    tax_price = profit * 841 // 10000
+elif tax == 3:
+    tax_price = profit * 33 // 1000
+elif tax == 4:
+    tax_price = profit * 841 // 10000 + profit * 33 // 1000
+else:
+    tax_price = 0
+
+profit = profit - practice_price - tax_price
 
 print("예상 월급은 : {}원입니다\n\n".format(profit))
 print("{}원으로 무엇을 할 수 있을까요?".format(profit))
